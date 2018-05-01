5a9617a2f1a42150d3669e28aba3ba9ce4db1d18
Kushal K Dey <kshldey@gmail.com>
2018-05-01 09:45:12 -0500

    refurbished the aRchaic directory with a simpler modular coding interface

--- R/model_archaic.R
+++ R/model_archaic.R
@@ -8,9 +8,9 @@
 #' 
 #' @param K the number of clusters to fit to the model.
 #' @param tol The tolerance level of convergence of the GoM model fit
-#' @param labs The labels used to group the samples in visualization. May be used to distinguish
+#' @param labs The factor of labels used to group the samples in visualization. 
+#'             May be used to distinguish
 #'             samples from different labs, or different library prep.
-#' @param levels The levels of the \code{labs} vector. Defaults to all unique values of \code{labs} vector.
 #' @param gom_method The GoM method type. Defaults to \code{independent} model proposed by
 #'                   Y. Shiraichi and M. Stephens. The other option is to use the \code{full}
 #'                   model which is uses the \code{maptpx} package by Matt Taddy.
@@ -18,7 +18,7 @@
 #' @param output_dir The output directory where the model is saved. 
 #'                   If NULL, it picks the current working directory.      
 #'                   
-#' @return Fits the GoM model on the aggregated data from \code{process_archaic}
+#' @return Fits the GoM model on the aggregated data from \code{prepare_archaic}
 #'   and then saves the model output as a .RData file. Also outputs model
 #'   assessment scores like the BIC. 
 #'   
@@ -27,11 +27,10 @@
 #' @export             
 
 
-model_aRchaic <- function(dirs,
+model_archaic <- function(dat,
                           K,
-                          tol=100,
+                          tol=0.1,
                           labs = NULL,
-                          levels = NULL,
                           gom_method = "independent",
                           gom.control = list(),
                           output_dir = NULL){
@@ -42,35 +41,77 @@ model_aRchaic <- function(dirs,
                                  light = 1, method_admix = 1, sample_init = TRUE, tmax = 10000)
   
   gom.control <- modifyList(gom.control.default, gom.control)
-  
-  datalist <- list()
-  for(numdir in 1:length(dirs)){
-    if(file.exists(paste0(folders[numdir], tail(strsplit(folders[numdir], "/")[[1]],1), ".rda"))){
-      datalist[[numdir]] <- get(load(paste0(folders[numdir], tail(strsplit(folders[numdir], "/")[[1]],1), ".rda")))
-      cat("Read .RData file from the folder, ", folders[numdir], "\n")
-    }else{
-      
+  if(class(dat) == "list"){
+    if(is.null(labs)) labs <- c()
+    cat("The data is read as a list of matrices - processed by prepare_archaic() \n")
+    datalist <- dat
+    for(numdir in 1:length(datalist)){
+      labs <- c(labs, rep(names(datalist)[numdir], dim(datalist[[numdir]])[1]))
     }
-  }
-  
-  sig_names <- colnames(datalist[[1]])
-  row_names_pool <- rownames(datalist[[1]])
-  if(length(datalist) >= 2){
-    for(num in 2:length(datalist)){
-      sig_names <- union(sig_names, colnames(datalist[[num]]))
-      row_names_pool <- c(row_names_pool, rownames(datalist[[num]]))
+    sig_names <- colnames(datalist[[1]])
+    row_names_pool <- rownames(datalist[[1]])
+    if(length(datalist) >= 2){
+      for(num in 2:length(datalist)){
+        sig_names <- union(sig_names, colnames(datalist[[num]]))
+        row_names_pool <- c(row_names_pool, rownames(datalist[[num]]))
+      }
     }
+    
+    pooled_data <- matrix(0, length(row_names_pool), length(sig_names))
+    rownames(pooled_data) <- row_names_pool
+    colnames(pooled_data) <- sig_names
+    for(num in 1:length(datalist)){
+      pooled_data[match(rownames(datalist[[num]]), rownames(pooled_data)), 
+                  match(colnames(datalist[[num]]), sig_names)] <- as.matrix(datalist[[num]])
+    }
+  }else if(class(dat) == "character"){
+    if(is.null(labs)) labs <- c()
+    message("The data is read as names of folders")
+    folders <- dat
+    datalist <- list()
+    for(numdir in 1:length(folders)){
+      if(file.exists(paste0(folders[numdir], tail(strsplit(folders[numdir], "/")[[1]],1), ".rda"))){
+        datalist[[numdir]] <- get(load(paste0(folders[numdir], tail(strsplit(folders[numdir], "/")[[1]],1), ".rda")))
+        cat("Successfully read .RData file from the folder, ", folders[numdir], "CHECK : \n")
+      }else{
+        message(".RData file not found in folder", folders[numdir], "running 
+             prepare_archaic on the MFF files in the folder")
+        proc_out <- prepare_aRchaic(folders[numdir])
+        datalist[[numdir]] <- proc_out
+      }
+      labs <- c(labs, rep(tail(strsplit(folders[numdir], "/")[[1]],1), dim(datalist[[numdir]])[1]))
+    }
+    
+    sig_names <- colnames(datalist[[1]])
+    row_names_pool <- rownames(datalist[[1]])
+    if(length(datalist) >= 2){
+      for(num in 2:length(datalist)){
+        sig_names <- union(sig_names, colnames(datalist[[num]]))
+        row_names_pool <- c(row_names_pool, rownames(datalist[[num]]))
+      }
+    }
+    
+    pooled_data <- matrix(0, length(row_names_pool), length(sig_names))
+    rownames(pooled_data) <- row_names_pool
+    colnames(pooled_data) <- sig_names
+    for(num in 1:length(datalist)){
+      pooled_data[match(rownames(datalist[[num]]), rownames(pooled_data)), 
+                  match(colnames(datalist[[num]]), sig_names)] <- as.matrix(datalist[[num]])
+    }
+  }else if (class(dat) == "matrix" | class(dat) == "data.frame"){
+    pooled_data <- dat
+    if(is.null(labs)) labs <- 1:nrow(pooled_data)
+    if(is.null(levels)) levels <- labs
+    if (min( pooled_data %% 1) !=0 ) stop ("dat must be a counts data matrix ")
+    if (min(pooled_data) < 0 ) stop ("dat must contain only positive integers ")
+  }else{
+    stop("class of the dat input must be a character or a vector of characters
+          indicating dorectory names, or a 
+           matrix of data frame of counts")
   }
   
-  pooled_data <- matrix(0, length(row_names_pool), length(sig_names))
-  rownames(pooled_data) <- row_names_pool
-  colnames(pooled_data) <- sig_names
-  
-  # temp_pooled_data <- matrix(0, length(row_names_pool), length(sig_names))
-  
-  for(num in 1:length(datalist)){
-    pooled_data[match(rownames(datalist[[num]]), rownames(pooled_data)), match(colnames(datalist[[num]]), sig_names)] <- as.matrix(datalist[[num]])
-  }
+  if(length(labs) != dim(pooled_data)[1]) stop(paste0("the length of labs is :", length(labs),
+          "which does not match the dimension of the data matrix compiled: ", dim(pooled_data)[1]))
   
   zero_sum_rows <- which(rowSums(pooled_data) == 0)
   if(length(zero_sum_rows) > 0){
@@ -79,10 +120,11 @@ model_aRchaic <- function(dirs,
   }
   
   pooled_data_2 <- filter_out_strand(pooled_data)
+  
+  
   if(gom_method == "full"){
     message("Fitting the Grade of Membership Model - full version - due to Matt Taddy")
     suppressWarnings(topic_clus <- do.call(maptpx::topics, append(list(counts = pooled_data_2, K=K, tol=tol, model = "full", signatures = NULL), topics.control)))
-    save(topic_clus, file = paste0(output_dir, "model.rda"))
   }else if(gom_method == "independent"){
     message("Fitting the Grade of Membership Model - independent version - due to Y. Shiraichi and M. Stephens")
     
@@ -98,7 +140,7 @@ model_aRchaic <- function(dirs,
     pos <- t(sapply(1:length(signature_set), function(x)
     {
       y = strsplit(signature_set[x], "")[[1]]
-      return(paste(y[12:length(y)], collapse=""))
+      return(paste(y[10:length(y)], collapse=""))
     }))
     
     
@@ -120,12 +162,15 @@ model_aRchaic <- function(dirs,
     suppressWarnings(topic_clus <- do.call(maptpx::topics, 
         append(list(counts = pooled_data_2, K=K, tol=tol, 
               model = "full", signatures = signatures), gom.control)))
-    save(topic_clus, file = paste0(output_dir, "model.rda"))
   }
   
   model_assessment <- CountClust::compGoM(pooled_data_2, topic_clus)
-  ll <- list("gom_model" = topic_clus,
-             "gom_assess" = model_assessment)
+  ll <- list("omega" = topic_clus$omega,
+             "theta" = topic_clus$theta,
+             "assessment" = model_assessment,
+             "labs" = labs)
+  if(is.null(output_dir)){ output_dir <- paste0(getwd(),"/")}
+  save(ll, file = paste0(output_dir, "model.rda"))
   return(ll)
 }
 
