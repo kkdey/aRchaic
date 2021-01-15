6ec330301b1c95b3bef0cfd682da5a83d2cd4e0d
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-01-15 13:07:28 -0600

    Deleted duplicate files, fixed the vignette, and built the pkgdown site.

--- vignettes/test.R
+++
@@ -1,9 +0,0 @@
-
-
-dirs <- c("inst/extdata/moderns", "inst/extdata/ancients")
-out <- prepare_archaic(dirs, delete = TRUE)
-model <- model_archaic(out, K = 2, output_dir = "inst/extdata/archaic_results")
-viz <- plot_archaic(model, output_dir = "inst/extdata/archaic_results",
-                    background = "modern")
-
-
